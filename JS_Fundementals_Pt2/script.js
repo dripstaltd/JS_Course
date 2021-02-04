e5cd24eacc8fa7d55e41483050fd579fb757e1bf
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-02-04 11:48:20 +0000

    Part 2 Fundementals JS
    Learnt about strict mode and how it can visually help me become aware of  bugs as they happen (console).
    
    'use strict';
    
    Also it stops us from using words reserved by ECMA for future use in JavaScript code, so we dont break the web later on ;P

---
+++ JS_Fundementals_Pt2/script.js
@@ -0,0 +1,16 @@
+'use strict'; 
+// has to be the very first line of code to use Strict Mode. (Secures Code) creates visual errors and forbids us to do certain things;
+
+let hasDriversLicense = false;
+const passTest = true;
+
+// We want to write some logic so that that when they pass the test hasDriversLicense changes to true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log('I can drive');
+
+// RESERVED WORDS FOR FUTURE USE BY JAVASCRIPT
+// const interface = 'Audio';
+// const private = 534;
+// const if = 534;
+
