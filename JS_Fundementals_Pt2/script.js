b392651300a2847cbe4427a1bd35eb2336b97d96
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-02-08 15:51:07 +0000

    Update script.js

--- JS_Fundementals_Pt2/script.js
+++ JS_Fundementals_Pt2/script.js
@@ -1,24 +1,30 @@
 'use strict'; 
-/*
 
-? FUNCTIONS
+// ! ALERTS & TITLES  
+// TODO
+// * Notes
+// Standard Comments
 
 
-has to be the very first line of code to use Strict Mode. (Secures Code) creates visual errors and forbids us to do certain things;
 
-let hasDriversLicense = false;
-const passTest = true;
 
-// We want to write some logic so that that when they pass the test hasDriversLicense changes to true;
+//!   FUNCTIONS    
 
-if (passTest) hasDriversLicense = true;
-if (hasDriversLicense) console.log('I can drive');
+// * has to be the very first line of code to use Strict Mode. (Secures Code) creates visual errors and forbids us to do certain things;
 
-// RESERVED WORDS FOR FUTURE USE BY JAVASCRIPT
-// const interface = 'Audio';
-// const private = 534;
-// const if = 534;
-*/
+// let hasDriversLicense = false;
+// const passTest = true;
+
+// TODO: We want to write some logic so that that when they pass the test hasDriversLicense changes to true;
+
+// if (passTest) hasDriversLicense = true;
+// if (hasDriversLicense) console.log('I can drive');
+
+//! RESERVED WORDS FOR FUTURE USE BY JAVASCRIPT 
+// * const interface = 'Audio';
+// * const private = 534;
+// * const if = 534;
+
 
 // function logger() {FUNCTION BUDDY}
 // function logger() {
@@ -29,31 +35,30 @@ if (hasDriversLicense) console.log('I can drive');
 // logger();
 // logger();
 // logger();
+
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// * NOTE: Creates the values of apples, oranges as juice
+// }
+// Sets the values of apples, oranges and saves the value under a variable.
+// const appleJuice = fruitProcessor(5, 0); 
+// console.log(appleJuice);
+// // console.log(fruitProcessor(5, 0));
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+// TODO; DRY CODE: 
+// TODO; Reusable code so to not repeat ourselves.
+
+
+//!                    F U N C T I O N S                    
+//! D E C L A R A T I O N S    &    E X P R E S S I O N S   
 /*
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-   // Creates the values of apples, oranges as juice
-}
-
-const appleJuice = fruitProcessor(5, 0); // Sets the values of apples, oranges and saves the value under a variable.
-console.log(appleJuice);
-// console.log(fruitProcessor(5, 0));
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-// DRY CODE: Reusable code so to not repeat ourselfs.
-*/
-
-
-//   / / / / / / / / / / / / / /
-//      F U N C T I O N S    / /
-//  D E C L A R A T I O N S  / /
-//             &             / /
-//   E X P R E S S I O N S   / /
-//   / / / / / / / / / / / / / /
-
-// D E C L A R A T I O N: Giving the function a name
+//TODO: Functions are values that can be stored inside a variable;
+//! Declaration:
+//* Can be called before they are defined
+//* Giving the function a name
 // (declaring the function) (nameTheFunction) (function parameter) { (code) (return ) }
 function calcAge1(birthYear) {
   return 2037 - birthYear;
@@ -61,10 +66,33 @@ function calcAge1(birthYear) {
 // (functionName) (Function Arguements / which fills in the function parameter placeholder)
 const age1 = calcAge1(1991);
 
-// / / / / / / / / / / / / / / / / / / / / / / / / / /
-// E X P R E S S I O N: No name (anonymous function)
+
+//! E X P R E S S I O N:  
+//* No name (anonymous function)
 
 const calcAge2 = function (birthYear) {
   return 2037 - birthYear;
 }
 const age2 = calcAge2(1991);
+console.log(age1, age2);
+*/
+
+//! ARROW FUNCTIONS:  
+//* Good for executing 1 parameter and 1 line of code;
+//* Shorter function expression;
+//*
+//*
+// Arrow function
+const calcAge3 = birthYear => 2021 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2021 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+}
+console.log (yearsUntilRetirement(1991, 'Ben'));
+
+
