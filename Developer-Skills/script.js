1688afe2e4bb0fa12e41a8917291daf9c84ab838
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-02-19 12:17:30 +0000

    Number to Strink
    let str = '';
    str += `${arr[i]}°C in ${i + 1} days ... `;

--- Developer-Skills/script.js
+++ Developer-Skills/script.js
@@ -39,6 +39,7 @@
 // is 7 > 3) true so max is now 7
 
 //-| USING BREAKPOINT IN SOURCES to Debug   ;
+/*
 const measureKelvin = function () {
   const measurement = {
     type: 'temp',
@@ -58,8 +59,9 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY BUG
 console.log(measureKelvin());
-
+*/
 //-| USING CHROME DEBUGGER TO FIX BUG      ;
+/*
 const calcTempAmplitudeBug = function (t1, t2) {
   const temps = t1.concat(t2);
 
@@ -83,3 +85,64 @@ const calcTempAmplitudeBug = function (t1, t2) {
 const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
 // BUG IDENTIFY
 console.log(amplitudeBug);
+*/
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+*/
+//:Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+/*
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+*/
+//? TEST DATA 1: [17, 21, 23]
+//? TEST DATA 2: [12, 5, -5, 0, 4]
+
+//> 1) Understanding the problem
+//> - Array transformed to string, separated by ...
+//> - What is the X days? Answer: index + 1
+
+//> 2) Breaking up into sub-problems
+//> - Transform array into string
+//> - Transform each element to string with ºC
+//> - Strings needs to contain day (index + 1)
+//> - Add ... between elements and start and end of string
+//> - Log string to console
+
+/*
+const t1 = [17, 21, 23];
+const t2 = [12, 5, -5, 0, 4];
+
+const printForecast = function (arr) {
+  for (let i = 0; i < arr.length; i++) {
+    const tmp = arr[i];
+    const days = i + 1;
+    const temp = String(arr[i]);
+    if (days === 1) {
+      console.log(`... ${tmp}°C in ${days} day ...`);
+    } else {
+      console.log(`... ${tmp}°C in ${days} days ...`);
+    }
+  }
+};
+
+console.log(printForecast(t1, t2));
+*/
+
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+
+console.log(`... ${data1[0]}°C ... ${data1[1]}°C... ${data1[3]}°C...`);
+
+const printForecast = function (arr) {
+  let str = '';
+  for (let i = 0; i < arr.length; i++) {
+    str += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log('... ' + str);
+};
+printForecast(data1);
