dfcebcb59e4c28c07bb4fc19cbd35218109bf2ce
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-02-19 11:30:31 +0000

    CHROME Debugger

--- Developer-Skills/script.js
+++ Developer-Skills/script.js
@@ -38,12 +38,14 @@
 // moves to next position of loop
 // is 7 > 3) true so max is now 7
 
+//-| USING BREAKPOINT IN SOURCES to Debug   ;
 const measureKelvin = function () {
   const measurement = {
     type: 'temp',
     unit: 'celsius',
     // C) FIX BUG
-    value: Number(prompt('Degrees celsius:')),
+    // value: Number(prompt('Degrees celsius:')),
+    value: 10,
   };
   // B) FIND BUG
   console.table(measurement);
@@ -56,3 +58,28 @@ const measureKelvin = function () {
 };
 // A) IDENTIFY BUG
 console.log(measureKelvin());
+
+//-| USING CHROME DEBUGGER TO FIX BUG      ;
+const calcTempAmplitudeBug = function (t1, t2) {
+  const temps = t1.concat(t2);
+
+  let max = 0;
+  let min = 0;
+
+  // let max = temps[0];
+  // let min = temps[0];
+
+  for (let i = 0; i < temps.length; i++) {
+    const curTemp = temps[i];
+    if (typeof curTemp !== 'number') continue;
+
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
+  }
+  console.log(max, min);
+  return max - min;
+};
+
+const amplitudeBug = calcTempAmplitudeBug([3, 5, 1], [9, 4, 5]);
+// BUG IDENTIFY
+console.log(amplitudeBug);
