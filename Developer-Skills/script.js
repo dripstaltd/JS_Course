befcda3f470b4f329fa42086c43892d99568a3cf
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-02-19 11:06:58 +0000

    Finding Bugs and Fixing

---
+++ Developer-Skills/script.js
@@ -0,0 +1,58 @@
+// Remember, we're gonna use strict mode in all scripts now!
+'use strict';
+
+// const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+// const temperatures2 = [45, -2, -6, -1, 'error', 9, 13, -8, 15, 14, 9, 5];
+
+// 1) Understanding the Problem
+// - What is temp amplitude? Answer: difference between highest and lowest temp
+// - How to compute max and min temperatures
+// What is a sensor error? And what to do?
+
+// 2) Breaking up into sub-problems
+// - How to ignore errors?
+// - Find max value in temp arr
+// - Find min value in temp arr
+// - Subtract min from max (amplitude) and return it
+
+// const calcTempAmplitudeNew = function (t1, t2) {
+//   const temps = t1.concat(t2);
+
+//   let max = temps[0];
+//   let min = temps[0];
+//   for (let i = 0; i < temps.length; i++) {
+//     const curTemp = temps[i];
+//     if (typeof curTemp !== 'number') continue;
+
+//     if (curTemp > max) max = curTemp;
+//     if (curTemp < min) min = curTemp;
+//   }
+//   return max - min;
+// };
+
+// const amplitudeNew = calcTempAmplitudeNew(temperatures, temperatures2);
+// console.log(amplitudeNew);
+
+// max = 3
+// is 3 > 3) false
+// moves to next position of loop
+// is 7 > 3) true so max is now 7
+
+const measureKelvin = function () {
+  const measurement = {
+    type: 'temp',
+    unit: 'celsius',
+    // C) FIX BUG
+    value: Number(prompt('Degrees celsius:')),
+  };
+  // B) FIND BUG
+  console.table(measurement);
+  // console.log(measurement.value);
+  // console.warn(measurement.value);
+  // console.error(measurement.value);
+
+  const kelvin = measurement.value + 273;
+  return kelvin;
+};
+// A) IDENTIFY BUG
+console.log(measureKelvin());
--- Playground/playground.js
+++ Playground/playground.js
@@ -1,20 +1,28 @@
-'use strict';
+"use strict";
 
 // > Declaring a couple things first, using 'let' as they will change with user input;
 
 let ns;
 let dw;
-let bs; 
-let tc; 
+let bs;
+let tc;
 const bsS = (10, 25, 30, 50, 60, 70, 80, 100, 120, 150, 200);
 
-
-//-| Final Calculation  
-const nel = (a, b, c) => (a * b / c);
+//-| Final Calculation
+const nel = (a, b, c) => (a * b) / c;
 
 //-| Pre Calculations depending on user input
-function bAdjust(a, b, c) {
-  
-}
+function bAdjust(a, b, c) {}
 
-
+//>                                                                        ;
+//-| Coding challenge ideas:                                               ;
+//>                                                                        ;
+//>                                                                        ;
+//> -Challenge #1:                                                         ;
+//>                                                                        ;
+//> Determine students current stage of the course                         ;
+//> Set an empty variable tag called 'tag' ?                               ;
+//> Place them in the same pool as other students at the same level        ;
+//> Students must work together to help each other before asking for help  ;
+//> But not students who are further ahead than they are                   ;
+//>                                                                        ;
