20542c411187d1d26180e4c0b65b96090e86b419
dripstaltd <45140460+dripstaltd@users.noreply.github.com>
2021-03-16 16:38:52 +0000

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -1,19 +1,29 @@
 'use strict';
+
+const poll = {
+  registerNewAnswer: promptInput => inputNum => {},
+};
+
+const pollBtn = document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer);
+
+/*
 ///////////////////////////////////////////////
 const heathrow = {
-  airline: 'Heathrow',
+  airline: 'London Heathrow',
   iataCode: 'LH',
   bookings: [],
   book(flightNum, name) {
     console.log(
-      `${name} booked a seat on ${this.airline} flight ${this.iataCode} ${flightNum}`
+      `${name} booked a seat on ${this.airline} flight ${this.iataCode}${flightNum}`
     );
     this.bookings.push({ flight: `${this.iataCode} ${flightNum}, ${name}` });
   },
 };
 
-heathrow.book(239, 'Benjamin McCarthy');
-heathrow.book(666, 'Donald Duck');
+// heathrow.book(239, 'Benjamin McCarthy');
+// heathrow.book(666, 'Donald Duck');
 // console.log(heathrow);
 
 const eurowings = {
@@ -28,26 +38,70 @@ const book = heathrow.book;
 // book(23, 'Giselle McCarthy');
 
 // Call method
-book.call(eurowings, 23, 'Giselle McCarthy');
-book.call(heathrow, 239, 'Gareth McCarthy');
-console.log(eurowings);
-console.log(heathrow);
+// book.call(eurowings, 23, 'Giselle McCarthy');
+// book.call(heathrow, 239, 'Gareth McCarthy');
+// console.log(eurowings);
+// console.log(heathrow);
 
 const swiss = {
   airline: 'Swiss Air Lines',
   iataCode: 'LX',
   bookings: [],
 };
-book.call(swiss, 787, 'Lewis McCarthy');
+// book.call(swiss, 787, 'Lewis McCarthy');
 
 // Apply method takes an array
 const flightData = [583, 'George Cooper'];
 book.apply(swiss, flightData);
-console.log(swiss);
+// console.log(swiss);
 
 // Call method taking array
 book.call(swiss, ...flightData);
 
+// Bind method
+const bookEW = book.bind(eurowings);
+const bookLH = book.bind(heathrow);
+const bookLX = book.bind(swiss);
+// bookEW(29, 'Steven Williams');
+
+// Book function with 23 already defined
+const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Benjamin McCarthy');
+
+// With Event Listeners
+heathrow.planes = 300;
+heathrow.buyPlane = function () {
+  // console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', heathrow.buyPlane.bind(heathrow));
+
+//> Partial application;
+// Preset the parameters
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+
+console.log(addVAT(100));
+///////////////////////////////////////////////
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+const ukTax = addTaxRate(0.23);
+console.log(ukTax(300));
+///////////////////////////////////////////////
+const addVA = rate => value => value + value * rate;
+const uSATax = addVA(0.3);
+console.log(uSATax(300));
+*/
 ///////////////////////////////////////////////
 /*
 const greet = function (greeting) {
